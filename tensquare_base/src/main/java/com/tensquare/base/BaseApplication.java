b7057b301d1727950c42491fc1a01551d05cfe27
TianCi.Xiong <tiancixiong@163.com>
2020-10-13 16:11:16 +0800

    搭建基础微服务模块

---
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -0,0 +1,23 @@
+package com.tensquare.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * 启动类
+ * @author TianCi.Xiong
+ * @since 2020/10/13 15:46
+ **/
+@SpringBootApplication
+public class BaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
+    }
+}
