a7738bbbdce27f1e3fdd59e798293c2f67bf831b
TianCi.Xiong <tiancixiong@163.com>
2020-10-14 14:19:29 +0800

    - 搭建招聘微服务模块 - 实现企业CRUD功能 - 实现职位CRUD功能

---
+++ tensquare_recruit/src/main/java/com/tensquare/recruit/controller/RecruitController.java
@@ -0,0 +1,108 @@
+package com.tensquare.recruit.controller;
+
+import java.util.Map;
+
+import enums.ResultEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tensquare.recruit.pojo.Recruit;
+import com.tensquare.recruit.service.RecruitService;
+
+import entity.PageResult;
+import entity.Result;
+
+/**
+ * recruit控制器层
+ * @author Administrator
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/recruit")
+public class RecruitController {
+
+    @Autowired
+    private RecruitService recruitService;
+
+
+    /**
+     * 查询全部数据
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll() {
+        return new Result(true, ResultEnum.QUERY_SUCCESS.getCode(), ResultEnum.QUERY_SUCCESS.getMsg(), recruitService.findAll());
+    }
+
+    /**
+     * 根据ID查询
+     * @param id ID
+     * @return
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Result findById(@PathVariable String id) {
+        return new Result(true, ResultEnum.QUERY_SUCCESS.getCode(), ResultEnum.QUERY_SUCCESS.getMsg(), recruitService.findById(id));
+    }
+
+
+    /**
+     * 分页+多条件查询
+     * @param searchMap 查询条件封装
+     * @param page      页码
+     * @param size      页大小
+     * @return 分页结果
+     */
+    @RequestMapping(value = "/search/{page}/{size}", method = RequestMethod.POST)
+    public Result findSearch(@RequestBody Map searchMap, @PathVariable int page, @PathVariable int size) {
+        Page<Recruit> pageList = recruitService.findSearch(searchMap, page, size);
+        return new Result(true, ResultEnum.QUERY_SUCCESS.getCode(), ResultEnum.QUERY_SUCCESS.getMsg(), new PageResult<Recruit>(pageList.getTotalElements(), pageList.getContent()));
+    }
+
+    /**
+     * 根据条件查询
+     * @param searchMap
+     * @return
+     */
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    public Result findSearch(@RequestBody Map searchMap) {
+        return new Result(true, ResultEnum.QUERY_SUCCESS.getCode(), ResultEnum.QUERY_SUCCESS.getMsg(), recruitService.findSearch(searchMap));
+    }
+
+    /**
+     * 增加
+     * @param recruit
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public Result add(@RequestBody Recruit recruit) {
+        recruitService.add(recruit);
+        return new Result(true, ResultEnum.ADD_SUCCESS.getCode(), ResultEnum.ADD_SUCCESS.getMsg());
+    }
+
+    /**
+     * 修改
+     * @param recruit
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Result update(@RequestBody Recruit recruit, @PathVariable String id) {
+        recruit.setId(id);
+        recruitService.update(recruit);
+        return new Result(true, ResultEnum.EDIT_SUCCESS.getCode(), ResultEnum.EDIT_SUCCESS.getMsg());
+    }
+
+    /**
+     * 删除
+     * @param id
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Result delete(@PathVariable String id) {
+        recruitService.deleteById(id);
+        return new Result(true, ResultEnum.DEL_SUCCESS.getCode(), ResultEnum.DEL_SUCCESS.getMsg());
+    }
+
+}
---
+++ tensquare_recruit/src/main/java/com/tensquare/recruit/dao/EnterpriseDao.java
@@ -0,0 +1,17 @@
+package com.tensquare.recruit.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.tensquare.recruit.pojo.Enterprise;
+
+import java.util.List;
+
+/**
+ * 企业数据访问接口
+ * @author TianCi.Xiong
+ * @since 2020/10/14 12:48
+ **/
+public interface EnterpriseDao extends JpaRepository<Enterprise, String>, JpaSpecificationExecutor<Enterprise> {
+
+}
